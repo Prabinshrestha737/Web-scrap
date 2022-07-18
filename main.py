265725aecb57949c83a25a383ee8b6e11aeb2dbe
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-18 23:22:31 +0545

    find element with id

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-
+import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium import webdriver
 
+# os.environ['PATH'] += "C:\chromedriver"
 
-driver = webdriver.Chrome()
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://wordpress.org/download/")
+# my_element = driver.find_element("id", "download-the-app-windows-64")
+driver.implicitly_wait(2)
+my_element = driver.find_element_by_id('download-wordpress')
+my_element.click()
+
+my_second_element = driver.find_element_by_id()
