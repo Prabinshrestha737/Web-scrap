a88d1677e261b930936c46d5efa5018628975c53
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-19 23:19:00 +0545

    no major learning

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
-from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # os.environ['PATH'] += "C:\chromedriver"
 
@@ -14,4 +16,13 @@ driver.implicitly_wait(2)
 my_element = driver.find_element_by_id('download-wordpress')
 my_element.click()
 
-my_second_element = driver.find_element_by_id()
+# my_second_element = driver.find_element_by_id('sadasd')
+
+
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'apps-mobile')# Element Filteration
+        # The expected text
+
+    )
+)
