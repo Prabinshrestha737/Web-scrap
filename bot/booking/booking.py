4aa37b8a12c471303bfd07d3908b01ed13672abd
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-21 23:22:17 +0545

    working on deal finding

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -1,16 +1,42 @@
+from select import select
 from selenium import webdriver
 import os 
 import booking.constants as const 
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"C:\SeleniumDrivers"):
-        self.driver_path = driver_path
-        os.environ['PATH'] += self.driver_path
+    def __init__(self, teardown=False):
+        self.teardown = teardown
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
+        self.maximize_window()
 
+    def __exit__(self, exc_type,exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
 
     def land_first_page(self):
         self.get(const.BASE_URL)
-    
 
+    
+    def change_currency(self, currency=None):
+        currency_element = self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your currency"]'
+        )
+
+        currency_element.click()
+
+        selected_currency_element = self.find_element_by_css_selector(
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
+
+        selected_currency_element.click()
+
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element_by_id('ss')
+        search_field.clear()
+        search_field.send_keys(place_to_go)
+
+
+    
--- bot/run.py
+++ bot/run.py
@@ -1,6 +1,13 @@
+from locale import currency
 from booking.booking import Booking
 
-inst = Booking()
 
-inst.land_first_page()
+with Booking() as bot:
+    bot.land_first_page()
+    # bot.change_currency(currency='GBP')
+    bot.select_place_to_go("New York")
+
+
+
+
 
