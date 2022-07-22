063d603982d55bf995fbf22ac9edb0325b59fbaf
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-23 00:12:02 +0545

    date search

--- bot/run.py
+++ bot/run.py
@@ -6,8 +6,8 @@ with Booking() as bot:
     bot.land_first_page()
     # bot.change_currency(currency='GBP')
     bot.select_place_to_go("New York")
-
-
+    bot.select_dates(check_in_date='2022-07-24',
+    check_out_date='2022-07-26')
 
 
 
