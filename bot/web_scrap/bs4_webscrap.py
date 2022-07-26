c4190247f0852834c062c30e50653fadcd7446ce
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-26 09:08:32 +0545

    bs4 webscapping tree structure

--- bot/web_scrap/bs4_webscrap.py
+++ bot/web_scrap/bs4_webscrap.py
@@ -1,17 +1,19 @@
+from unittest import result
 from bs4 import BeautifulSoup
 
+import requests
 
-with open("index.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
+# with open("index.html", "r") as f:
+#     doc = BeautifulSoup(f, "html.parser")
 
 
 # print(doc)
 # print(doc.prettify())
 
 
-tag = doc.title
-print(tag)
-print(tag.string)
+# tag = doc.title
+# print(tag)
+# print(tag.string)
 
 
 #change string in tag variable
@@ -21,11 +23,19 @@ print(tag.string)
 
 # print(doc)
 
-
-tags = doc.find_all("p")[0]
-inside_tag = tags.find_all('b')
-
+# tags = doc.find_all("p")[0]
+# inside_tag = tags.find_all('b')
 
 
+url = "https://www.newegg.com/p/1DW-00GP-00392?Description=1060%20ti&cm_re=1060_ti-_-9SIAC85HD45687-_-Product"
 
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+# print(doc.prettify())
+
+prices = doc.find(text="$")
+
+parent = prices.parent
+strong = parent.find("strong")
+print(strong.string)
 
